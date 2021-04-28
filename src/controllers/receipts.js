5d8f0165715cadabb4a4aecf232c448748424bc2
msmaiaa <roxx77344@gmail.com>
2021-04-28 03:54:07 -0300

    feat: create boilerplate for receipts functions

---
+++ src/controllers/receipts.js
@@ -0,0 +1,40 @@
+
+
+const createReceipt = async (req, res, next) => {
+    try{
+
+    }catch(e){
+
+    }
+}
+
+const editReceipt = async (req, res, next) => {
+    try{
+
+    }catch(e){
+        
+    }
+}
+
+const getReceipts = async (req, res, next) => {
+    try{
+
+    }catch(e){
+        
+    }
+}
+
+const deleteReceipt = async (req, res, next) => {
+    try{
+
+    }catch(e){
+        
+    }
+}
+
+module.exports = {
+    createReceipt,
+    editReceipt,
+    getReceipts,
+    deleteReceipt
+}
---
+++ src/services/ReceiptsService.js
@@ -0,0 +1,17 @@
+const db = require('../helpers/database')
+
+const create = async(data) =>{
+    
+}
+
+const edit = async(data) =>{
+
+}
+
+const list = async(data) =>{
+
+}
+
+const del = async(data) =>{
+
+}
