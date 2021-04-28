15d32235ddd86d3e7cc4b2ddc7599e51539c8802
msmaiaa <roxx77344@gmail.com>
2021-04-28 08:52:59 -0300

    feat: add function to insert data into mysql table

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -13,7 +13,26 @@ const findSpecificRow = async ({ table, params }) => {
   return rows
 }
 
+const insertIntoTable = async ( table, values ) =>{
+  const conn = await connectDB()
+  let query = `INSERT INTO `
+  let vals;
+  switch (table) {
+    case "expenses":{
+      query += `expenses(description, value, is_fixed, due_date, category_id, user_id) VALUES (?, ?, ?, ?, ?, ?)`
+      vals = [values.description, values.value, values.is_fixed, values.due_date, values.category_id, values.user_id]
+    }
+  }
+  const res = await conn.query(query, vals)
+  if(res[0]){
+    return 200
+  }else{
+    return 400
+  }
+}
+
 module.exports = {
   selectAll,
-  findSpecificRow
+  findSpecificRow,
+  insertIntoTable
 }
