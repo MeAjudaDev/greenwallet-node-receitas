598b97f9e964a69723a0d053e8852925e3b81c07
msmaiaa <roxx77344@gmail.com>
2021-05-04 14:56:16 -0300

    refactor: add return structure to the endpoints

--- src/controllers/receipts.js
+++ src/controllers/receipts.js
@@ -3,31 +3,30 @@ const ReceiptsService = require('../services/ReceiptsService')
 const createReceipt = async (req, res, next) => {
     try{
         const status = await ReceiptsService.create(req.body)
-        return res.status(status).json({message: 'Request done'})
+        return res.status(status).json({message: 'Success'})
     }catch(e){
-        return res.status(400).json({message: e.message})
+        return res.status(400).json({message: 'Error while trying to create receipt', error: e.message})
     }
 }
 
 const editReceipt = async (req, res, next) => {
     try {
         const update = await ReceiptsService.edit(req.body)
-        const status = update.affectedRows == 1 ? { code:200, message: 'Request done'} : { code: 500, message: "Couldn't edit any receipt with the given id" } 
+        const status = update.affectedRows == 1 ? { code:200, message: 'Success'} : { code: 500, message: "Couldn't edit any receipt with the given id" } 
         return res.status(status.code).json({ message: status.message })
     
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({ err: error })
+        return res.status(500).json({ message: 'Error while trying to edit receipt', error: error.message })
     }
 }
 
 const getSingleReceipt = async (req, res, next) => {
     try{
         const receipt = await ReceiptsService.list({data:req.params, single:true})
-        const status = receipt.length > 0 ? { code: 200, message: receipt[0] } : { code: 500, message: "Couldn't find a receipt with the given id" }
-        return res.status(status.code).json({ message: status.message })
+        const status = receipt.length > 0 ? { code: 200, message: 'Success', body: receipt[0] } : { code: 500, message: "Couldn't find a receipt with the given id" }
+        return res.status(status.code).json({ message: status.message, body: status.body })
     }catch(e){
-        return res.status(400).json({message: e.message})
+        return res.status(400).json({message: 'Error while trying to get a single receipt', error: e.message})
     }
 }
 
@@ -37,20 +36,20 @@ const getMultipleReceipts = async (req, res, next) => {
         if(receipts.length < 1){
             return res.status(500).json({ message: "Couldn't find any receipts with the given parameters"})
         }else{
-            return res.status(200).json({receipts})
+            return res.status(200).json({ message: 'Success', body: receipts})
         }
     }catch(e){
-        return res.status(400).json({message: e.message})
+        return res.status(400).json({message:'Error while trying to get multiple receipts', error: e.message})
     }
 }
 
 const deleteReceipt = async (req, res, next) => {
     try{
         const del = await ReceiptsService.del({id})
-        const status = del.affectedRows == 1 ? { code:200, message: 'Request done!' } : { code: 500, message: "Couldn't find any receipt with the given id"}
+        const status = del.affectedRows == 1 ? { code:200, message: 'Delete with success' } : { code: 500, message: "Couldn't find any receipt with the given id"}
         return res.status(status.code).json({ message: status.message })
     }catch(e){
-        return res.json({message: e.message})
+        return res.json({message: 'Erro while trying to delete the receipt', error: e.message})
     }
 }
 
