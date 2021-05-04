605a972abd0c1f386a7e0be09b0994f8ce4b2114
msmaiaa <roxx77344@gmail.com>
2021-05-04 09:19:48 -0300

    refactor: remove validation from receipts controller

--- src/controllers/receipts.js
+++ src/controllers/receipts.js
@@ -23,9 +23,6 @@ const editReceipt = async (req, res, next) => {
 
 const getSingleReceipt = async (req, res, next) => {
     try{
-        if(!req.params.id){
-            return res.status(422).json({message: 'Missing params'})
-        }
         const receipt = await ReceiptsService.list({data:req.params, single:true})
         const status = receipt.length > 0 ? { code: 200, message: receipt[0] } : { code: 500, message: "Couldn't find a receipt with the given id" }
         return res.status(status.code).json({ message: status.message })
@@ -36,9 +33,6 @@ const getSingleReceipt = async (req, res, next) => {
 
 const getMultipleReceipts = async (req, res, next) => {
     try{
-        if(!req.query.user_id){
-            return res.status(422).json({message: 'Missing params'})
-        }
         const receipts = await ReceiptsService.list({data:req.query, single:false}) 
         if(receipts.length < 1){
             return res.status(500).json({ message: "Couldn't find any receipts with the given parameters"})
@@ -52,10 +46,6 @@ const getMultipleReceipts = async (req, res, next) => {
 
 const deleteReceipt = async (req, res, next) => {
     try{
-        const id = req.params.id
-        if(!id) {
-            return res.status(422).json({message: 'Invalid params'})
-        }
         const del = await ReceiptsService.del({id})
         const status = del.affectedRows == 1 ? { code:200, message: 'Request done!' } : { code: 500, message: "Couldn't find any receipt with the given id"}
         return res.status(status.code).json({ message: status.message })
