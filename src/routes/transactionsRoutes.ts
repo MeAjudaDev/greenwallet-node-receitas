41dd1274e3409ca7280112e9d111ea1e23e13f74
msmaiaa <roxx77344@gmail.com>
2021-06-15 19:08:58 -0300

    test: a

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -3,7 +3,7 @@ import TransactionsControllers from '../controllers/TransactionsControllers'
 import validate from '../validators/TransactionsValidators'
 
 const transactionsRoutes = Router()
-
+console.log('a')
 transactionsRoutes
   .get('/user/:user_id',
     validate.indexTransaction(),
